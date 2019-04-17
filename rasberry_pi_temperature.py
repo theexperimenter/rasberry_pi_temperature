7283d22167a6f216960f53aa961ade05a756ad1a
Debankur <debankur.ghosh@gmail.com>
2019-04-16 20:05:16 -0700

    Update rasberry_pi_temperature.py

--- rasberry_pi_temperature.py
+++ rasberry_pi_temperature.py
@@ -1,26 +1,3 @@
-"""
-Produces load on all available CPU cores
-"""
-from multiprocessing import Pool
-from multiprocessing import cpu_count
-import os
-import time
+import os 
 
-def f(x):
-    while True:
-        x*x
-
-def measure_temp():
-        temp = os.popen("vcgencmd measure_temp").readline()
-        return (temp.replace("temp=",""))
-
-if __name__ == '__main__':
-    processes = cpu_count()
-    print '-' * 20
-    print 'Running load on CPU'
-    print 'Utilizing %d cores' % processes
-    print '-' * 20
-    pool = Pool(processes)
-    pool.map(f, range(processes))
-    print(measure_temp())
-    time.sleep(1)
+os.system('while true; do vcgencmd measure_clock arm; vcgencmd measure_temp; sleep 10; done& stress -c 4 -t 900s')
